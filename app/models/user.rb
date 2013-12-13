d1f6e55cdefa631e3220d0dc2c22a5144885d792
= <=>
2013-12-13 11:54:30 -0800

    Write full user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,21 @@ class User < ActiveRecord::Base
   validates :email, :password_digest, :presence => true
   validates :email, :uniqueness => true
 
+  def self.find_by_credentials(email, secret)
+    @user = User.find_by_email(email)
+    if !!@user && @user.is_password?(secret)
+      return @user
+    else
+      raise "Invalid login information"
+    end
+  end
+
   def self.generate_session_token
     SecureRandom::urlsafe_base64(16)
+  end
+
+  def is_password?(secret)
+    BCrypt::Password.new(secret) == BCrypt::Password.new(self.password_digest)
   end
 
   def reset_session_token!
@@ -18,6 +31,10 @@ class User < ActiveRecord::Base
 
   def ensure_session_token
     self.session_token || self.reset_session_token!
+  end
+
+  def password=(secret)
+    self.password_digest = BCrypt::Password.create(secret)
   end
 
 end
