e471d5a809bd802d0a1245dceee2b3f6e188ec33
= <=>
2013-12-13 12:11:14 -0800

    Fix is_password? helper method

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,22 +19,22 @@ class User < ActiveRecord::Base
   end
 
   def is_password?(secret)
-    BCrypt::Password.new(secret) == BCrypt::Password.new(self.password_digest)
+    BCrypt::Password.new(self.password_digest).is_password?(secret)
   end
 
   def reset_session_token!
-    self.session_token = User.generate_session_token!
+    self.session_token = User.generate_session_token
     self.save!
+  end
+
+  def password=(secret)
+    self.password_digest = BCrypt::Password.create(secret)
   end
 
   private
 
   def ensure_session_token
     self.session_token || self.reset_session_token!
-  end
-
-  def password=(secret)
-    self.password_digest = BCrypt::Password.create(secret)
   end
 
 end
