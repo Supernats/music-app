f831e1c273dbdd227575891b96c27541d2285e86
App Academy Student <student@appacademy.io>
2013-12-13 11:20:01 -0800

    Initial skeleton commit. Begin first migration.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicApp::Application.config.secret_token = '4167c8b12eee4b1363f31afbfbd5e267d2362e78c8c279b00b67a0ca800656f07b03f92d2595d8c41874f791a5d78581d814a0af4e11d6fc8a8d0f0e1b594fce'
