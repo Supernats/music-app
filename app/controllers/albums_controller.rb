f45177f70ca9780cb906c1c0eadb4c45a881f7bc
Supernats <nathanseither@gmail.com>
2013-12-13 15:45:28 -0800

    Implement AlbumsController

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,31 +1,50 @@
 class AlbumsController < ApplicationController
 
   def index
-
+    render :index
   end
 
   def new
-
+    render :new
   end
 
   def show
-
+    render :show
   end
 
   def create
-
+    @album = Album.create(params[:album])
+    if !!@album
+      @album.save!
+      flash[:notices] = "The #{album.name} master just came in! Bitchin!"
+      redirect_to album_url(@album)
+    else
+      flash[:errors] = "You fried the board, bro! No bueno."
+      render :new
+    end
   end
 
   def edit
-
+    render :edit
   end
 
   def update
-
+    @album = Album.find(params[:id])
+    if @album.update_attributes
+      @album.save!
+      flash[:notices] = "The new mix on track 1 is killer!"
+      redirect_to album_url(@album)
+    else
+      flash[:errors] = "You can't mess with a masterpiece!"
+      render :edit
+    end
   end
 
   def destroy
-
+    @album = Album.find(params[:id])
+    @album.destroy!
+    flash[:notices] = "#{@album.name} is no more."
+    redirect_to new_band_album_url(@album)
   end
 
 end
