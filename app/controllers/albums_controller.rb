d0a0b0afa52db4499867f85ff88577b01e5bcc7d
Supernats <nathanseither@gmail.com>
2013-12-13 15:04:01 -0800

    skeleton controllers

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,3 +1,31 @@
 class AlbumsController < ApplicationController
 
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
