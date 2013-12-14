0047efc81c7ada0eec2b83814ee0c057e3b1f2ec
Supernats <nathanseither@gmail.com>
2013-12-13 16:17:55 -0800

    Add inclusion validations for Track#track_type and Album#venue

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,9 @@
 class Album < ActiveRecord::Base
   attr_accessible :band_id, :name, :venue
-  
+
   validates :band_id, :name, :venue, :presence => true
+  validates :venue, :inclusion => ["studio","live"]
+
 
   belongs_to :band
   has_many :tracks, :dependent => :destroy
--- app/models/track.rb
+++ app/models/track.rb
@@ -2,8 +2,8 @@ class Track < ActiveRecord::Base
   attr_accessible :album_id, :track_type
 
   validates :album_id, :track_type, :presence => true
+  validates :track_type, :inclusion => ["standard", "bonus"]
 
   belongs_to :album
   has_one :band, :through => :album
-
 end
